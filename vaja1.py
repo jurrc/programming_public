8e6221bf0d04d5cbb63c09c39ff8a4c30dabfa08
jure cucek <jure@jurecucek.si>
2020-03-09 22:56:12 -0700

    update

--- vaja1.py
+++ vaja1.py
@@ -1,14 +1,34 @@
 
 
-
 def func(d, m):
     if m in (1, 2, 3):
-        print('zima')
-    elif m in (4, 5, 6):
-        print('pomlad')
-    elif m in (7, 8, 9):
-        print('poletje')
+        if m == 3 and d >= 21:
+            funcpom(d, m)
+        else:
+            print('zima')
     else:
-        print('jesen')
+        funcpom(d, m)
+
+def funcpom(d, m):
+    if m in (4, 5, 6):
+        if m == 6 and d >= 21:
+            funcpol(d, m)
+        else:
+            print('pomlad')
+    else:
+        funcpol(d, m)
+
+def funcpol(d, m):
+    if m in (7, 8, 9):
+        if m == 9 and d >= 23:
+            funcjes(d, m)
+        else:
+            print('poletje')
+    else:
+        funcjes(d, m)
+
+def funcjes(d, m):
+    print('jesen')
 
 func(int(input('dan >> ')), int(input('mesec >> ')))
+
